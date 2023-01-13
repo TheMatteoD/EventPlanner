903a76c4174ca11bb0bc62d197d3ff94250d86d2
Matteo DeSimone <thematteod@gmail.com>
2023-01-13 10:16:09 -0500

    Added the persistence of edit,delete, and create to the database. Added loading

--- client-app/src/features/activities/dashboad/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboad/ActivityDashboard.tsx
@@ -14,14 +14,16 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({ activities, selectActivity, selectedActivity, cancelSelectActivity,
-editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
     return (
         <Grid>
             <Grid.Column width="10">
-                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}
+                submitting={submitting} />
             </Grid.Column>
             <Grid.Column width="6">
                 {selectedActivity && !editMode &&
@@ -29,7 +31,7 @@ editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
                     openForm={openForm}
                     />}
                     {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+                <ActivityForm submitting={submitting} closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
             </Grid.Column>
         </Grid>
     )
