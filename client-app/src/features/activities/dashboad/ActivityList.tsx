46c5ac191b3c377d193e4575e661637b2bd2e5d4
Matteo DeSimone <thematteod@gmail.com>
2023-01-14 23:03:24 -0500

    Added routing with React-Router

--- client-app/src/features/activities/dashboad/ActivityList.tsx
+++ client-app/src/features/activities/dashboad/ActivityList.tsx
@@ -1,11 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import { SyntheticEvent, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 
 export default observer(function ActivityList() {
     const { activityStore } = useStore();
-    const {deleteActivity, activitiesByDate, loading} = activityStore;
+    const { deleteActivity, activitiesByDate, loading } = activityStore;
 
     const [target, setTarget] = useState('');
 
@@ -27,7 +28,8 @@ export default observer(function ActivityList() {
                                 <div>{activity.city}, {activity.venue}</div>
                             </Item.Description>
                             <Item.Extra>
-                                <Button onClick={() => activityStore.selectActivity(activity.id)} floated='right' content='View' color='blue' />
+                                <Button as={Link} to={`/activities/${activity.id}`}
+                                    floated='right' content='View' color='blue' />
                                 <Button
                                     name={activity.id}
                                     loading={loading && target === activity.id}
